933f2c5a9891bd0c2519974d3d97dce64f6739ae
will492 <wilfredoelar4@gmail.com>
2024-06-12 10:01:04 +0800

    s

---
+++ main/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="header-section">
+            <div class="logo">
+                <img class="first-logo" src="img/logo.png" alt="First Logo">
+                <img class="second-logo" src="https://www.pngmart.com/files/7/Portfolio-PNG-Pic.png" alt="Second Logo">
+            </div>
+            
+            <nav class="headernav">
+                <div class="header-btn">
+                    <a href="#home">Home</a>
+                    <a href="#">Contact</a>
+                    <a href="#">project</a>
+                    <a href="#">Contact</a>
+                    <a href="#about">About Me</a>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <div id="home" class="cont-1">
+        <div class="content-1">
+                <img src="https://www.pngmart.com/files/7/Portfolio-PNG-Pic.png" alt="Second Logo">
+        </div>
+
+        <div class="content-11">
+            <h1>Welcome to My Portfolio</h1>
+            <p>As a fresh graduate with a degree in Information Technology, <br>I am eager to apply my technical skills and knowledge to real-world challenges, continuously learning and growing in the dynamic field of IT.</p>
+        </div>
+</div>
+
+    <div id="about" class="cont-2">
+
+        <div class="content-2">
+            <h1> 
+                <span>Hellow!</span> <br> <span class="name">I'm Wilfredo Jamindang</span>
+            </h1>
+            <p>
+                As a fresh graduate with a degree in Information Technology, I am eager to apply my technical skills and knowledge to real-world challenges, continuously learning and growing in the dynamic field of IT. Throughout my academic journey, I have gained hands-on experience with a variety of programming languages, software development tools, and IT infrastructures. I am particularly passionate about cybersecurity and software development, and I have completed several projects that showcase my abilities in these areas. My goal is to leverage my education and enthusiasm to contribute to innovative projects and collaborate with industry professionals who share my commitment to technology and excellence. I am excited about the opportunities ahead and look forward to making a meaningful impact in the IT industry.
+            </p>
+        </div>
+        <div class="photo">
+            <img src="img/pic2.jpg" alt="">
+        </div>
+    </div>
+
+    <footer>
+        <!-- Footer content goes here -->
+    </footer>
+    <script src="js/script.js"></script>
+</body>
+</html>
